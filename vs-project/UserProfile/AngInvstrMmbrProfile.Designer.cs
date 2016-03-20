c8b77932fe502e66b442f7eb19aff6ff2356b86e
SLjar <jaryaroslav@gmail.com>
2016-03-20 22:09:22 +0200

    Added files via upload
    US-6 - Брезвин
    Бізнес-ангел  можуть переглянути стартапи, погруповані по бізнес-інкубаторах

--- vs-project/UserProfile/AngInvstrMmbrProfile.Designer.cs
+++ vs-project/UserProfile/AngInvstrMmbrProfile.Designer.cs
@@ -28,21 +28,383 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.IMG = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_imin = new System.Windows.Forms.Label();
+            this.lbl_imax = new System.Windows.Forms.Label();
+            this.lbl_twitter = new System.Windows.Forms.Label();
+            this.lbl_phone = new System.Windows.Forms.Label();
+            this.lbl_skype = new System.Windows.Forms.Label();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.lbl_geoint = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.showprofile = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.startupslist = new System.Windows.Forms.Button();
+            this.pnl_startups = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.starname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lbl_iexperience = new System.Windows.Forms.Label();
+            this.pnl_profile = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.pnl_startups.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.pnl_profile.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(767, 36);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VM";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(684, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Logout";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.IMG);
+            this.panel2.Controls.Add(this.lbl_name);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(5, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(173, 178);
+            this.panel2.TabIndex = 1;
+            // 
+            // IMG
+            // 
+            this.IMG.AutoSize = true;
+            this.IMG.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IMG.Location = new System.Drawing.Point(55, 65);
+            this.IMG.Name = "IMG";
+            this.IMG.Size = new System.Drawing.Size(57, 29);
+            this.IMG.TabIndex = 2;
+            this.IMG.Text = "IMG";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(57, 154);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 137);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.lbl_imin);
+            this.panel3.Controls.Add(this.lbl_imax);
+            this.panel3.Controls.Add(this.lbl_twitter);
+            this.panel3.Controls.Add(this.lbl_phone);
+            this.panel3.Controls.Add(this.lbl_skype);
+            this.panel3.Controls.Add(this.lbl_email);
+            this.panel3.Controls.Add(this.lbl_geoint);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(4, 10);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(429, 147);
+            this.panel3.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(221, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Investition amount:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lbl_imin
+            // 
+            this.lbl_imin.AutoSize = true;
+            this.lbl_imin.Location = new System.Drawing.Point(311, 71);
+            this.lbl_imin.Name = "lbl_imin";
+            this.lbl_imin.Size = new System.Drawing.Size(25, 13);
+            this.lbl_imin.TabIndex = 11;
+            this.lbl_imin.Text = "imin";
+            // 
+            // lbl_imax
+            // 
+            this.lbl_imax.AutoSize = true;
+            this.lbl_imax.Location = new System.Drawing.Point(311, 58);
+            this.lbl_imax.Name = "lbl_imax";
+            this.lbl_imax.Size = new System.Drawing.Size(28, 13);
+            this.lbl_imax.TabIndex = 10;
+            this.lbl_imax.Text = "imax";
+            // 
+            // lbl_twitter
+            // 
+            this.lbl_twitter.AutoSize = true;
+            this.lbl_twitter.Location = new System.Drawing.Point(49, 97);
+            this.lbl_twitter.Name = "lbl_twitter";
+            this.lbl_twitter.Size = new System.Drawing.Size(35, 13);
+            this.lbl_twitter.TabIndex = 9;
+            this.lbl_twitter.Text = "twitter";
+            // 
+            // lbl_phone
+            // 
+            this.lbl_phone.AutoSize = true;
+            this.lbl_phone.Location = new System.Drawing.Point(49, 84);
+            this.lbl_phone.Name = "lbl_phone";
+            this.lbl_phone.Size = new System.Drawing.Size(37, 13);
+            this.lbl_phone.TabIndex = 7;
+            this.lbl_phone.Text = "phone";
+            // 
+            // lbl_skype
+            // 
+            this.lbl_skype.AutoSize = true;
+            this.lbl_skype.Location = new System.Drawing.Point(49, 71);
+            this.lbl_skype.Name = "lbl_skype";
+            this.lbl_skype.Size = new System.Drawing.Size(35, 13);
+            this.lbl_skype.TabIndex = 6;
+            this.lbl_skype.Text = "skype";
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Location = new System.Drawing.Point(49, 58);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(31, 13);
+            this.lbl_email.TabIndex = 5;
+            this.lbl_email.Text = "email";
+            // 
+            // lbl_geoint
+            // 
+            this.lbl_geoint.AutoSize = true;
+            this.lbl_geoint.Location = new System.Drawing.Point(49, 45);
+            this.lbl_geoint.Name = "lbl_geoint";
+            this.lbl_geoint.Size = new System.Drawing.Size(36, 13);
+            this.lbl_geoint.TabIndex = 4;
+            this.lbl_geoint.Text = "geoint";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Contact Information:";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel6.Controls.Add(this.showprofile);
+            this.panel6.Location = new System.Drawing.Point(5, 236);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(172, 39);
+            this.panel6.TabIndex = 5;
+            // 
+            // showprofile
+            // 
+            this.showprofile.Location = new System.Drawing.Point(50, 7);
+            this.showprofile.Name = "showprofile";
+            this.showprofile.Size = new System.Drawing.Size(75, 23);
+            this.showprofile.TabIndex = 0;
+            this.showprofile.Text = "My Profile";
+            this.showprofile.UseVisualStyleBackColor = true;
+            this.showprofile.Click += new System.EventHandler(this.showprofile_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel7.Controls.Add(this.startupslist);
+            this.panel7.Location = new System.Drawing.Point(6, 288);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(171, 36);
+            this.panel7.TabIndex = 6;
+            // 
+            // startupslist
+            // 
+            this.startupslist.Location = new System.Drawing.Point(49, 7);
+            this.startupslist.Name = "startupslist";
+            this.startupslist.Size = new System.Drawing.Size(75, 23);
+            this.startupslist.TabIndex = 0;
+            this.startupslist.Text = "Startups list";
+            this.startupslist.UseVisualStyleBackColor = true;
+            this.startupslist.Click += new System.EventHandler(this.startupslist_Click);
+            // 
+            // pnl_startups
+            // 
+            this.pnl_startups.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnl_startups.Controls.Add(this.listView1);
+            this.pnl_startups.Controls.Add(this.label2);
+            this.pnl_startups.Location = new System.Drawing.Point(183, 46);
+            this.pnl_startups.Name = "pnl_startups";
+            this.pnl_startups.Size = new System.Drawing.Size(589, 503);
+            this.pnl_startups.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.starname});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 45);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(399, 443);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // starname
+            // 
+            this.starname.Text = "starname";
+            this.starname.Width = 200;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "List of startups:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Controls.Add(this.lbl_iexperience);
+            this.panel4.Location = new System.Drawing.Point(4, 169);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(429, 103);
+            this.panel4.TabIndex = 3;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // lbl_iexperience
+            // 
+            this.lbl_iexperience.AutoSize = true;
+            this.lbl_iexperience.Location = new System.Drawing.Point(12, 11);
+            this.lbl_iexperience.Name = "lbl_iexperience";
+            this.lbl_iexperience.Size = new System.Drawing.Size(61, 13);
+            this.lbl_iexperience.TabIndex = 0;
+            this.lbl_iexperience.Text = "iexperience";
+            // 
+            // pnl_profile
+            // 
+            this.pnl_profile.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnl_profile.Controls.Add(this.panel3);
+            this.pnl_profile.Controls.Add(this.panel4);
+            this.pnl_profile.Location = new System.Drawing.Point(184, 46);
+            this.pnl_profile.Name = "pnl_profile";
+            this.pnl_profile.Size = new System.Drawing.Size(437, 278);
+            this.pnl_profile.TabIndex = 4;
             // 
             // AngInvstrMmbrProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_startups);
+            this.Controls.Add(this.pnl_profile);
             this.Name = "AngInvstrMmbrProfile";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Angel Investor Profile - Venture Market";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.AngInvstrMmbrProfile_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.pnl_startups.ResumeLayout(false);
+            this.pnl_startups.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.pnl_profile.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lbl_phone;
+        private System.Windows.Forms.Label lbl_skype;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.Label lbl_geoint;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_twitter;
+        private System.Windows.Forms.Label lbl_imin;
+        private System.Windows.Forms.Label lbl_imax;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button showprofile;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button startupslist;
+        private System.Windows.Forms.Panel pnl_startups;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lbl_iexperience;
+        private System.Windows.Forms.Panel pnl_profile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader starname;
+        private System.Windows.Forms.Label IMG;
     }
 }
--- vs-project/UserProfile/AngInvstrMmbrProfile.cs
+++ vs-project/UserProfile/AngInvstrMmbrProfile.cs
@@ -13,11 +13,114 @@ namespace code.UserProfile
     public partial class AngInvstrMmbrProfile : Form
     {
         User user;
+        int ind = 0;
+        DataClasses1DataContext vmDB = new DataClasses1DataContext();
+        //Startup startup;
         public AngInvstrMmbrProfile(User user)
         {
             InitializeComponent();
             this.user = user;
-            InitializeComponent();
+            DataClasses1DataContext vmDB = new DataClasses1DataContext();
+            var angel = user.AngelInvestors.SingleOrDefault(u => u.UserID == user.ID);
+            pnl_startups.Hide();
+            lbl_name.Text = string.Format("{0} {1}", user.FName, user.LName);
+            lbl_phone.Text = string.Format("Phone: {0}", angel.Phone);
+            lbl_email.Text = string.Format("Email: {0}", user.Email);
+            lbl_skype.Text = string.Format("Skype: {0}", angel.Skype);
+            lbl_geoint.Text = string.Format("Geo interests: {0}", angel.Geo_Inerests);
+            lbl_twitter.Text = string.Format("Twitter: {0}", angel.Twitter);
+            lbl_imax.Text = string.Format("Max: {0}", angel.Max_amount);
+            lbl_imin.Text = string.Format("Min: {0}", angel.Min_Amount);
+            lbl_iexperience.Text = string.Format("Investment experience: {0}", angel.Investment_Experience);
+        }
+
+        private void AngInvstrMmbrProfile_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            LoginForm icmp = new LoginForm();
+            icmp.Show();
+        }
+
+        private void showprofile_Click(object sender, EventArgs e)
+        {
+            ind = 0;
+            listView1.Items.Clear();
+            pnl_startups.Hide();
+            pnl_profile.Show();
+        }
+
+        private void startupslist_Click(object sender, EventArgs e)
+        {
+            if(ind == 1) { listView1.Items.Clear(); }
+            pnl_profile.Hide();
+            pnl_startups.Show();
+            string arr = null;
+            string dll = "sdsdds";
+            var jr = (from p in vmDB.Startups
+                      orderby p.IncubatorID
+
+                      join t in vmDB.Business_Incubators on p.IncubatorID equals t.ID
+
+                      select new { starname = p.Title, businame = t.Title, idb = p.IncubatorID });
+            foreach (var group in jr)
+            {
+                arr = group.businame;
+                ListViewGroup csharp_group = new ListViewGroup(arr);
+                listView1.Groups.Add(csharp_group);
+                var itm = new ListViewItem { Text = group.starname, Group = csharp_group };
+                listView1.Items.Add(itm);
+            }
+            ind = 1;
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sname = null;
+
+            if (listView1.SelectedIndices.Count <= 0)
+            {
+                return;
+            }
+            if (listView1.SelectedIndices.Count >= 0)
+            {
+                sname = listView1.SelectedItems[0].Text;
+                MessageBox.Show(sname);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sname = null;
+            string iname = null;
+
+            sname = listView1.SelectedItems[0].SubItems[0].Text;
+            iname = listView1.SelectedItems[0].SubItems[1].Text;
+            MessageBox.Show(sname + " , " + iname);
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
