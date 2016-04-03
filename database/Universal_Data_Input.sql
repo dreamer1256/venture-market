3df3d07358883c783708dd511d65ea068720bc76
SLjar <jaryaroslav@gmail.com>
2016-04-03 23:11:36 +0300

    Create universal data input script

---
+++ database/Universal_Data_Input.sql
@@ -0,0 +1,206 @@
+USE Venture_Market
+
+SET IDENTITY_INSERT Users ON
+INSERT Users(ID,Username,Email,Password,FName,LName) VALUES
+(1,N'ColinCornish',	N'colin.cornish@gmail.com', N'951357', N'Colin', N'Cornish'),
+(2,N'VirginiaBond',	N'virginia.bond@gmail.com', N'852654', N'Virginia', N'Bond'),
+(3,N'SallyClarkson', N'sally.clarkson@gmail.com', N'753951', N'Sally', N'Clarkson'),
+(4,N'LeonardDickens', N'leonard.dickens@gmail.com', N'987321', N'Leonard', N'Dickens'),
+(5,N'ThomasMay', N'thomas.may@gmail.com', N'456951', N'Thomas', N'May'),
+(6,N'DorothyInce', N'dorothy.ince@gmail.com', N'963741', N'Dorothy', N'Ince'),
+(7,N'DonnaMorrison', N'donna.morrison@gmail.com', N'852753', N'Donna', N'Morrison'),
+(8,N'JonathanHughes', N'jonathan.hughes@gmail.com', N'741963', N'Jonathan', N'Hughes'),
+(9,N'ChloeCampbell', N'chloe.campbell@gmail.com', N'8426842', N'Chloe', N'Campbell'),
+(10,N'MichelleLee', N'michelle.lee@gmail.com', N'957135', N'Michelle', N'Lee'),
+(11,N'TrevorGill', N'trevor.gill@gmail.com', N'397182', N'Trevor', N'Gill'),
+(12,N'VictoriaInce', N'victoria.ince@gmail.com', N'791346', N'Victoria', N'Ince'),
+(13,N'DavidJones', N'david.jones@gmail.com', N'957535', N'David', N'Jones'),
+(14,N'EdwardReid', N'edward.reid@gmail.com', N'759614', N'Edward', N'Reid'),
+(15,N'DonnaBuckland', N'donna.buckland@gmail.com', N'852545', N'Donna', N'Buckland'),
+(16,N'JackAvery', N'jack.avery@gmail.com', N'782398', N'Jack', N'Avery'),
+(17,N'JoeCornish', N'joe.cornish@gmail.com', N'983278', N'Joe', N'Cornish'),
+(18,N'JaneWright', N'jane.wright@gmail.com', N'129832', N'Jane', N'Wright'),
+(19,N'SallyNewman', N'sally.newman@gmail.com', N'45657595', N'Sally', N'Newman'),
+(20,N'FionaChapman', N'fiona.chapman@gmail.com', N'842695', N'Fiona', N'Chapman'),
+(21,N'GabrielleDavies', N'gabrielle.davies@gmail.com', N'951357', N'Gabrielle', N'Davies'),
+(22,N'ElizabethKelly',	N'elizabeth.kelly@gmail.com', N'852654', N'Elizabeth', N'Kelly'),
+(23,N'JoannePiper', N'joanne.piper', N'753951', N'Joanne', N'Piper'),
+(24,N'AustinPeters', N'austin.peters@gmail.com', N'987321', N'Austin', N'Peters'),
+(25,N'AmyOgden', N'amy.ogden@gmail.com', N'456951', N'Amy', N'Ogden'),
+(26,N'MichaelCampbell', N'michael.campbell@gmail.com', N'963741', N'Michael', N'Campbell'),
+(27,N'AndreaDavidson', N'andrea.davidson@gmail.com', N'852753', N'Andrea', N'Davidson'),
+(28,N'BenjaminMitchell', N'benjamin.mitchell@gmail.com', N'741963', N'Benjamin', N'Mitchell'),
+(29,N'DanParsons', N'dan.parsons@gmail.com', N'8426842', N'Dan', N'Parsons'),
+(30,N'ThomasAlsop', N'thomas.alsop@gmail.com', N'957135', N'Thomas', N'Alsop'),
+(31,N'JohnArnold', N'john.arnold@gmail.com', N'397182', N'John', N'Arnold'),
+(32,N'NatalieNolan', N'natalie.nolan@gmail.com', N'791346', N'Natalie', N'Nolan'),
+(33,N'ConnorBlack', N'connor.black@gmail.com', N'957535', N'Connor', N'Black'),
+(34,N'KarenBerry', N'karen.berry@gmail.com', N'759614', N'Karen', N'Berry'),
+(35,N'MollyWright', N'molly.wright@gmail.com', N'852545', N'Molly', N'Wright'),
+(36,N'DianePaige', N'diane.paige@gmail.com', N'782398', N'Diane', N'Paige'),
+(37,N'JacobVaughan', N'jacob.vaughan@gmail.com', N'983278', N'Jacob', N'Vaughan'),
+(38,N'AlisonMetcalfe', N'alison.metcalfe@gmail.com', N'129832', N'Alison', N'Metcalfe'),
+(39,N'RyanInce', N'ryan.ince@gmail.com', N'45657595', N'Ryan', N'Ince'),
+(40,N'WandaMorrison', N'wanda.morrison@gmail.com', N'842695', N'Wanda', N'Morrison')
+SET IDENTITY_INSERT Users OFF
+GO
+SET IDENTITY_INSERT dbo.Role ON 
+INSERT dbo.Role (ID, Role_Title, Role_Description) VALUES
+ (1, N'Angel_Investor', N'Бізнес-ангел'),
+ (2, N'IC_Member', N'Представник інвестиційної компанії'),
+ (3, N'StartupCEO', N'Керівник стартапу'),
+ (4, N'Invest_Manager', N'Інвестиційний менеджер'),
+ (5, N'Startup_Member', N'Учасник стартапу'),
+ (6, N'Admin', N'Адміністратор')
+SET IDENTITY_INSERT dbo.Role OFF
+GO
+INSERT User_Role(UserId,RoleID) VALUES
+(1,1),(2,2),(3,3),(4,4),(5,5),
+(6,6),(7,6),(8,1),(9,2),(10,3),
+(11,4),(12,5),(13,1),(14,2),(15,3),
+(16,4),(17,5),(18,1),(19,2),(20,3),
+(21,4),(22,5),(23,1),(24,2),(25,3),
+(26,4),(27,5),(28,2),(29,3),(30,4),
+(31,5),(32,1),(33,2),(34,3),(35,4),
+(36,5),(37,1),(38,2),(39,3),(40,4)
+GO
+SET IDENTITY_INSERT AngelInvestor ON
+INSERT AngelInvestor(ID,UserID,Geo_Inerests,Skype) VALUES
+(1,10,N'USA',N'michelle.lee'),
+(2,5,N'USA',N'thomas.may'),
+(3,3,N'Ukraine',N'sally.clarkson'),
+(4,2,N'UK',N'virginia.bond'),
+(5,8,NULL,N'jonathan.hughes'),
+(6,17,N'Poland',N'joe.cornish')
+SET IDENTITY_INSERT AngelInvestor OFF
+GO
+SET IDENTITY_INSERT Industry_Interests_List ON
+INSERT Industry_Interests_List (ID, Title) VALUES
+(1,N'eCommerce'),
+(2,N'Cloud'),
+(3,N'Media'),
+(4,N'Mobile'),
+(5,N'Saas'),
+(6,N'Games'),
+(7,N'Research'),
+(8,N'Geo'),
+(9,N'Social Networking'),
+(10,N'Advertisment')
+SET IDENTITY_INSERT Industry_Interests_List OFF
+GO
+INSERT Angel_Interests(InterestID, AngelID) Values
+(10, 5),
+(6, 5),
+(7, 5),
+(1, 1),
+(5, 1),
+(2, 2),
+(8, 2),
+(1, 3),
+(9, 3),
+(3, 4),
+(4, 4),
+(6, 6),
+(1, 6),
+(4, 6)
+GO
+SET IDENTITY_INSERT Business_Incubator ON
+INSERT Business_Incubator (ID, Title, Number_Of_Seats, Address, Website) VALUES
+(1,N'LvivInvest',50,N'Ukraine Lviv',N'lvivI.com'),
+(2,N'PLINC',300,N'Poland Lodz',N'PLINC.com'),
+(3,N'Allianz',130,N'Germany Munchen',N'Allianz.com')
+SET IDENTITY_INSERT Business_Incubator OFF
+GO
+SET IDENTITY_INSERT Development_Stage ON
+INSERT Development_Stage (ID, Stage) VALUES
+(1,N'seed stage'),
+(2,N'startup stage'),
+(3,N'growth stage'),
+(4,N'expansion stage'),
+(5,N'exit stage')
+SET IDENTITY_INSERT Development_Stage OFF
+GO
+SET IDENTITY_INSERT Startup ON
+INSERT Startup(ID,Title,Business_Model,Competitors,Marketing_Strategy,Total_Investment,Website,IncubatorID,Development_StageID) VALUES
+(1,N'Cardiomo',N'Fee in',NULL,N'Product',0,N'Cardiomo.com',2,2),
+(2,N'VoltyCO',N'Free in',3,N'Product',0,N'Volty.com',1,1),
+(3,N'Green Wheels',N'Franchise',2,N'pricing',0,N'WheelsG.com',1,2),
+(4,N'intubus',N'Free in',5,N'Product',0,N'intubus.com',3,2),
+(5,N'mymobstr',N'Free in',4,N'Product',0,N'mymo.com',1,1),
+(6,N'Infinity',N'Free in',5,N'Product',0,N'Infinity-W.com',NULL,1),
+(7,N'NoName',N'Free in',1,N'Franchise',0,N'NoName-club.com',NULL,1),
+(8,N'Ark',N'Free in',3,N'Product',0,N'ark.com',NULL,1),
+(9,N'Arpen',N'Free in',2,N'Franchise',0,N'Arpen.com',NULL,1)
+SET IDENTITY_INSERT Startup OFF
+GO
+SET IDENTITY_INSERT Investment_Company ON
+INSERT Investment_Company (ID,Title,Website,Office_address) Values
+(1,N'LvivInvest',N'LvivIn.com',N'Ukraine Lviv'),
+(2,N'WestINC',N'WINC.com',N'Ukraine Dolyna'),
+(3,N'KievCorp',N'KievC.com',N'Ukraine Kiev')
+SET IDENTITY_INSERT Investment_Company OFF
+GO
+SET IDENTITY_INSERT Investment_Manager ON
+INSERT Investment_Manager(ID,Investment_CompanyID,UserID) VALUES
+(1,2,4),
+(2,3,1),
+(3,1,6),
+(4,2,11),
+(5,2,12),
+(6,3,13),
+(7,3,14),
+(8,1,15),
+(9,1,16)
+SET IDENTITY_INSERT Investment_Manager OFF
+GO
+SET IDENTITY_INSERT Application ON
+INSERT Application (ID,ManagerID,StartupID,State,Application_Round) VALUES
+(1,2,5,N'considered',1),
+(2,1,4,N'accepted',1),
+(3,4,3,N'rejected',2),
+(4,3,2,N'rejected',1),
+(5,9,1,N'accepted',1),
+(6,NULL,6,N'no state',0),
+(7,NULL,7,N'no state',0),
+(8,NULL,8,N'no state',0),
+(9,NULL,9,N'no state',0)
+SET IDENTITY_INSERT Application OFF
+GO
+INSERT Round_Of_Funding (ID,StartupID,Title,Total_Investment) VALUES
+(1,1,N'Round1',10000),
+(2,2,N'Round2',15000),
+(3,3,N'Round1',5000),
+(4,4,N'Round2',13000),
+(5,5,N'Round1',0)
+GO
+INSERT Round_Investor (RoundID,CompanyID,AngelID) VALUES
+(1,3,NULL),
+(2,NULL,6),
+(3,1,NULL),
+(4,2,NULL),
+(5,NULL,3)
+GO
+SET IDENTITY_INSERT Startup_Members ON
+INSERT Startup_Members (ID,StartupID,UserID,Country,Is_CEO,Skype) VALUES
+(1,1,7,N'Canada',1,N'donna.morrison'),
+(2,1,9,N'Canada',0,N'chloe.campbell'),
+(3,1,18,N'Canada',0,NULL),
+(4,1,19,N'Canada',0,NULL),
+(5,2,20,N'Poland',1,N'FionaChapman'),
+(6,2,21,N'Poland',0,NULL),
+(7,2,22,N'Poland',0,N'elizabeth.kelly'),
+(8,2,23,N'Poland',0,NULL),
+(9,2,24,N'Poland',0,NULL),
+(10,3,25,N'Ukraine',1,N'amy.ogden'),
+(11,3,26,N'Ukraine',0,N'michael.campbell'),
+(12,3,27,N'Ukraine',0,N'andrea.davidson'),
+(13,4,28,N'USA',1,N'benjamin.mitchell'),
+(14,4,29,N'USA',0,NULL),
+(15,4,30,N'USA',0,N'thomas.alsop'),
+(16,4,31,N'USA',0,N'john.arnold'),
+(17,5,32,N'UK',1,N'natalie.nolan'),
+(18,5,33,N'UK',0,NULL),
+(19,5,34,N'UK',0,N'karen.berry'),
+(20,5,35,N'UK',0,NULL)
+SET IDENTITY_INSERT Startup_Members OFF
+SELECT * FROM Startup_Members
