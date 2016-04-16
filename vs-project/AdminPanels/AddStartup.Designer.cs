5ca81da746c95506ab15d756f9783130bc855962
Berezhnyj <berez95@mail.ru>
2016-04-16 21:46:19 +0300

    The administrator can add a startup
    The administrator can add a startup in entering all the information about it

--- vs-project/AdminPanels/AddStartup.Designer.cs
+++ vs-project/AdminPanels/AddStartup.Designer.cs
@@ -28,11 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbox_bis_model = new System.Windows.Forms.ComboBox();
+            this.tb_title = new System.Windows.Forms.TextBox();
+            this.cbox_mark_strategy = new System.Windows.Forms.ComboBox();
+            this.tb_description = new System.Windows.Forms.TextBox();
+            this.tb_website = new System.Windows.Forms.TextBox();
+            this.tb_twitter = new System.Windows.Forms.TextBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Accept = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(257, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(257, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(257, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Business_Model";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(257, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Marketing_Strategy";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Location = new System.Drawing.Point(257, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Website";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(257, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Twitter";
+            // 
+            // cbox_bis_model
+            // 
+            this.cbox_bis_model.FormattingEnabled = true;
+            this.cbox_bis_model.Items.AddRange(new object[] {
+            "Free in"});
+            this.cbox_bis_model.Location = new System.Drawing.Point(372, 164);
+            this.cbox_bis_model.Name = "cbox_bis_model";
+            this.cbox_bis_model.Size = new System.Drawing.Size(121, 21);
+            this.cbox_bis_model.TabIndex = 6;
+            // 
+            // tb_title
+            // 
+            this.tb_title.Location = new System.Drawing.Point(372, 84);
+            this.tb_title.Name = "tb_title";
+            this.tb_title.Size = new System.Drawing.Size(100, 20);
+            this.tb_title.TabIndex = 7;
+            // 
+            // cbox_mark_strategy
+            // 
+            this.cbox_mark_strategy.FormattingEnabled = true;
+            this.cbox_mark_strategy.Location = new System.Drawing.Point(372, 191);
+            this.cbox_mark_strategy.Name = "cbox_mark_strategy";
+            this.cbox_mark_strategy.Size = new System.Drawing.Size(121, 21);
+            this.cbox_mark_strategy.TabIndex = 8;
+            // 
+            // tb_description
+            // 
+            this.tb_description.Location = new System.Drawing.Point(372, 110);
+            this.tb_description.Name = "tb_description";
+            this.tb_description.Size = new System.Drawing.Size(100, 20);
+            this.tb_description.TabIndex = 9;
+            // 
+            // tb_website
+            // 
+            this.tb_website.Location = new System.Drawing.Point(372, 218);
+            this.tb_website.Name = "tb_website";
+            this.tb_website.Size = new System.Drawing.Size(100, 20);
+            this.tb_website.TabIndex = 10;
+            // 
+            // tb_twitter
+            // 
+            this.tb_twitter.Location = new System.Drawing.Point(372, 247);
+            this.tb_twitter.Name = "tb_twitter";
+            this.tb_twitter.Size = new System.Drawing.Size(100, 20);
+            this.tb_twitter.TabIndex = 11;
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Cancel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Cancel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_Cancel.Location = new System.Drawing.Point(400, 337);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(150, 36);
+            this.btn_Cancel.TabIndex = 13;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // btn_Accept
+            // 
+            this.btn_Accept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Accept.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Accept.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_Accept.Location = new System.Drawing.Point(239, 337);
+            this.btn_Accept.Name = "btn_Accept";
+            this.btn_Accept.Size = new System.Drawing.Size(150, 36);
+            this.btn_Accept.TabIndex = 12;
+            this.btn_Accept.Text = "Add";
+            this.btn_Accept.UseVisualStyleBackColor = true;
+            this.btn_Accept.Click += new System.EventHandler(this.btn_Accept_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(372, 135);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(137, 20);
+            this.dateTimePicker.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label7.Location = new System.Drawing.Point(257, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Foundation Date";
+            // 
+            // AddStartup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(830, 473);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Accept);
+            this.Controls.Add(this.tb_twitter);
+            this.Controls.Add(this.tb_website);
+            this.Controls.Add(this.tb_description);
+            this.Controls.Add(this.cbox_mark_strategy);
+            this.Controls.Add(this.tb_title);
+            this.Controls.Add(this.cbox_bis_model);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddStartup";
             this.Text = "AddStartup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbox_bis_model;
+        private System.Windows.Forms.TextBox tb_title;
+        private System.Windows.Forms.ComboBox cbox_mark_strategy;
+        private System.Windows.Forms.TextBox tb_description;
+        private System.Windows.Forms.TextBox tb_website;
+        private System.Windows.Forms.TextBox tb_twitter;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Accept;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label7;
     }
 }
--- vs-project/AdminPanels/AddStartup.cs
+++ vs-project/AdminPanels/AddStartup.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace code.AdminPanels
 {
-    public partial class AddStartup : Form
+
+    public partial class 
+        AddStartup : Form
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public AddStartup()
         {
             InitializeComponent();
+        }
+        
+    private void btn_Accept_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext vmDB = new DataClasses1DataContext();
+              var st = vmDB.Startups.Select(s => s.Business_Model);
+              var str = vmDB.Startups.Select(b => b.Marketing_Strategy);
+               foreach (var s in st)
+           // string[] a = { "Free in" , "Franchise" };
+                cbox_bis_model.Items.Add(s);
+               foreach (var b in str)
+           // string[] b = { "Product", "Franchise" };
+            cbox_mark_strategy.Items.Add(b);
+
+            if (tb_title.Text == "" || tb_description.Text == "" || tb_website.Text == "" || tb_twitter.Text == "" || cbox_bis_model.Text == "" || cbox_mark_strategy.Text == "")
+                MessageBox.Show("Fields can not be empty!\n",
+                    "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                Startup star = new Startup
+                {
+                    Title = tb_title.Text,
+                    Description = tb_description.Text,
+                    Business_Model = cbox_bis_model.Text,
+                    Marketing_Strategy = cbox_mark_strategy.Text,
+                    Website = tb_website.Text,
+                    Twitter = tb_twitter.Text,
+                    Competitors = null,
+                    Total_Investment = 0,
+                    Foundation_Date =  dateTimePicker.Value,
+                    ceoID = null,
+                    IncubID = null,
+                    DevStageID = 1
+                };
+
+                vmDB.Startups.InsertOnSubmit(star);
+                try
+                {
+                    vmDB.SubmitChanges();
+                    logger.Info("Startup " + tb_title.Text + " added to the system");
+                    MessageBox.Show("Startup successfully added to the system!",
+                        "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Hide();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    logger.Error("An error ocured while adding Startup to DB: " + ex.Message);
+                }
+            }
         }
     }
 }
