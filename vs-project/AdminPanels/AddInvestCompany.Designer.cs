e6599f23ecb11427d4bbcb215b4175cb3b0ffe46
SLjar <jaryaroslav@gmail.com>
2016-04-17 12:17:56 +0300

    The administrator can add invest company
    The administrator can add invest company

--- vs-project/AdminPanels/AddInvestCompany.Designer.cs
+++ vs-project/AdminPanels/AddInvestCompany.Designer.cs
@@ -28,11 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.date_of_foundation_inv_company = new System.Windows.Forms.DateTimePicker();
+            this.txt_addinvest_comName = new System.Windows.Forms.TextBox();
+            this.txt_addinvest_decription = new System.Windows.Forms.TextBox();
+            this.txt_addinvest_website = new System.Windows.Forms.TextBox();
+            this.txt_addinvest_adress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_comp_ceo = new System.Windows.Forms.TextBox();
+            this.btn_confirm = new System.Windows.Forms.Button();
+            this.btn_Cancel2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(101, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Company name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(101, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(101, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Website:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(101, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Foundation date:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(101, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Office adress:";
+            // 
+            // date_of_foundation_inv_company
+            // 
+            this.date_of_foundation_inv_company.Location = new System.Drawing.Point(238, 163);
+            this.date_of_foundation_inv_company.Name = "date_of_foundation_inv_company";
+            this.date_of_foundation_inv_company.Size = new System.Drawing.Size(148, 20);
+            this.date_of_foundation_inv_company.TabIndex = 5;
+            // 
+            // txt_addinvest_comName
+            // 
+            this.txt_addinvest_comName.Location = new System.Drawing.Point(238, 55);
+            this.txt_addinvest_comName.Name = "txt_addinvest_comName";
+            this.txt_addinvest_comName.Size = new System.Drawing.Size(148, 20);
+            this.txt_addinvest_comName.TabIndex = 6;
+            // 
+            // txt_addinvest_decription
+            // 
+            this.txt_addinvest_decription.Location = new System.Drawing.Point(238, 81);
+            this.txt_addinvest_decription.Multiline = true;
+            this.txt_addinvest_decription.Name = "txt_addinvest_decription";
+            this.txt_addinvest_decription.Size = new System.Drawing.Size(148, 45);
+            this.txt_addinvest_decription.TabIndex = 7;
+            // 
+            // txt_addinvest_website
+            // 
+            this.txt_addinvest_website.Location = new System.Drawing.Point(238, 134);
+            this.txt_addinvest_website.Name = "txt_addinvest_website";
+            this.txt_addinvest_website.Size = new System.Drawing.Size(148, 20);
+            this.txt_addinvest_website.TabIndex = 8;
+            // 
+            // txt_addinvest_adress
+            // 
+            this.txt_addinvest_adress.Location = new System.Drawing.Point(238, 193);
+            this.txt_addinvest_adress.Name = "txt_addinvest_adress";
+            this.txt_addinvest_adress.Size = new System.Drawing.Size(148, 20);
+            this.txt_addinvest_adress.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label6.Location = new System.Drawing.Point(101, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "CEO:";
+            // 
+            // txt_comp_ceo
+            // 
+            this.txt_comp_ceo.Location = new System.Drawing.Point(238, 224);
+            this.txt_comp_ceo.Name = "txt_comp_ceo";
+            this.txt_comp_ceo.Size = new System.Drawing.Size(148, 20);
+            this.txt_comp_ceo.TabIndex = 11;
+            // 
+            // btn_confirm
+            // 
+            this.btn_confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_confirm.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_confirm.Location = new System.Drawing.Point(93, 270);
+            this.btn_confirm.Name = "btn_confirm";
+            this.btn_confirm.Size = new System.Drawing.Size(148, 31);
+            this.btn_confirm.TabIndex = 12;
+            this.btn_confirm.Text = "Confirm";
+            this.btn_confirm.UseVisualStyleBackColor = true;
+            this.btn_confirm.Click += new System.EventHandler(this.btn_confirm_Click);
+            // 
+            // btn_Cancel2
+            // 
+            this.btn_Cancel2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Cancel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Cancel2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_Cancel2.Location = new System.Drawing.Point(258, 270);
+            this.btn_Cancel2.Name = "btn_Cancel2";
+            this.btn_Cancel2.Size = new System.Drawing.Size(142, 31);
+            this.btn_Cancel2.TabIndex = 14;
+            this.btn_Cancel2.Text = "Cancel";
+            this.btn_Cancel2.UseVisualStyleBackColor = true;
+            this.btn_Cancel2.Click += new System.EventHandler(this.btn_Cancel2_Click);
+            // 
+            // AddInvestCompany
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(499, 344);
+            this.Controls.Add(this.btn_Cancel2);
+            this.Controls.Add(this.btn_confirm);
+            this.Controls.Add(this.txt_comp_ceo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_addinvest_adress);
+            this.Controls.Add(this.txt_addinvest_website);
+            this.Controls.Add(this.txt_addinvest_decription);
+            this.Controls.Add(this.txt_addinvest_comName);
+            this.Controls.Add(this.date_of_foundation_inv_company);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddInvestCompany";
             this.Text = "AddInvestCompany";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker date_of_foundation_inv_company;
+        private System.Windows.Forms.TextBox txt_addinvest_comName;
+        private System.Windows.Forms.TextBox txt_addinvest_decription;
+        private System.Windows.Forms.TextBox txt_addinvest_website;
+        private System.Windows.Forms.TextBox txt_addinvest_adress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_comp_ceo;
+        private System.Windows.Forms.Button btn_confirm;
+        private System.Windows.Forms.Button btn_Cancel2;
     }
 }
--- vs-project/AdminPanels/AddInvestCompany.cs
+++ vs-project/AdminPanels/AddInvestCompany.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace code.AdminPanels
 {
     public partial class AddInvestCompany : Form
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public AddInvestCompany()
         {
             InitializeComponent();
+        }
+
+        private void btn_confirm_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext vmDB = new DataClasses1DataContext();
+            bool isunique = true;
+            var snm = from us in vmDB.Investment_Companies
+                      select us;
+            foreach (var us in snm)
+            {
+                if (us.Title == txt_addinvest_comName.Text)
+                {
+                    isunique = false;
+                }
+            }
+                if (txt_addinvest_adress.Text == "" || txt_addinvest_comName.Text == "" || txt_addinvest_decription.Text == "" || txt_addinvest_website.Text == "")
+                { MessageBox.Show("Some fields are epmty!\n", "Warning", MessageBoxButtons.OK); }
+                else if(isunique == false)
+                { MessageBox.Show("Such company exist.\n", "Warning", MessageBoxButtons.OK);}
+                else
+                {
+                    Investment_Company addcomp = new Investment_Company
+                    {
+                        Title = txt_addinvest_comName.Text,
+                        Description = txt_addinvest_decription.Text,
+                        Website = txt_addinvest_website.Text,
+                        Foundation_Date = date_of_foundation_inv_company.Value,
+                        Office_Address = txt_addinvest_adress.Text,
+                        CEO = txt_comp_ceo.Text
+                    };
+
+                    vmDB.Investment_Companies.InsertOnSubmit(addcomp);
+                    try
+                    {
+                        vmDB.SubmitChanges();
+                        logger.Info("Company " + txt_addinvest_comName.Text + " added to the system");
+                        MessageBox.Show("Company successfully added to the system!",
+                            "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Hide();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        logger.Error("An error ocured while adding Company to DB: " + ex.Message);
+                    }
+                }
+        }
+
+        private void btn_Cancel2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
