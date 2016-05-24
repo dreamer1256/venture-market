b7444460e6ea38e40e5c6b6603f9e1abc778d4b8
Berezhnyj <berez95@mail.ru>
2016-05-24 19:31:08 +0300

    Bug fixed

--- vs-project/RegisterForms/Signup_StartupMember.Designer.cs
+++ vs-project/RegisterForms/Signup_StartupMember.Designer.cs
@@ -66,6 +66,7 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.Controls.Add(this.lbl_chbx_is);
             this.panel1.Controls.Add(this.label2);
